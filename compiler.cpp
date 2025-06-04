76924bfe22c059f5a135b678835d93d5656c8cdb
ZuggRugg <shayne.tman@gmail.com>
2025-06-04 16:30:43 -0500

    added addition operation, finished basic prototype

--- compiler.cpp
+++ compiler.cpp
@@ -1,3 +1,8 @@
+/////////////////////////////////
+// Program Created By ZuggRugg //
+// Date: 6/4/2025              //
+/////////////////////////////////
+
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -21,40 +26,32 @@ struct token {
 //function definitions
 std::string tokenize();
 void get_tokens(std::string rec, std::vector<token>& t_answer);
+int expr(std::vector<token>& t_answer, int& pos);
+bool eat(std::vector<token>& tokens, int& pos, Token_Type t);
 
 // main function
 int main(void) {
+  while(1) {
+  int pos = 0;
+
   std::string answer;
   std::vector<token> t_answer; //vector containing tokens
 
-  std::cout << "please input an expression to be evaluated: ";
+  std::cout << "Idle_Gnosis> ";
   getline(std::cin,answer);
   get_tokens(answer, t_answer);
+  
 
-}
-
-//note to ignore this function for now
-//simple tokenizer that breaks up sstream into individual tokens
-std::string tokenize(std::string rec) {
-  Token_Type type;
-  std::string s;
-
-  switch(type) {
-  case NUMBER:    s += "NUMBER";    break;
-  case PLUS:      s += "PLUS";      break;
-  case EOF_:      s += "EOF";       break;
+  std::cout << "final answer = " << expr(t_answer, pos) << "\n";
   }
-
-  return s;
 }
-
 
 //iterates through string and classifies each char as a simple token
 void get_tokens(std::string rec, std::vector<token>& t_answer) {
   for (int i = 0; i < rec.length(); i++) {
     
     if (rec[i] == '+') { //check if its + operator
-      t_answer.push_back({PLUS, 000});
+      t_answer.push_back({PLUS, 999});
     }
 
     if(isdigit(rec[i])) { 
@@ -62,13 +59,40 @@ void get_tokens(std::string rec, std::vector<token>& t_answer) {
       t_answer.push_back({NUMBER, number});
     }
 
-    if(i == rec.length()-1) { t_answer.push_back({EOF_, 000}); } // add end of file token
+    if(i == rec.length()-1) { t_answer.push_back({EOF_, 999}); } // add end of file token
   }
-  
+
   //read tokens
   for(int i = 0; i < t_answer.size(); i++) {
     std::cout << t_answer[i].type << " - " << t_answer[i].value << "\n";
   }
 }
+
+
+// pre-defined expr function to eval expressions like '3+5'
+int expr(std::vector<token>& t_answer, int& pos) {
+    token left = t_answer[0];
+    if(!eat(t_answer, pos, NUMBER)) { exit(1); }
+
+    token Op = t_answer[1];
+    if(!eat(t_answer, pos, PLUS)) { exit(1); }
+
+    token right = t_answer[2];
+    if(!eat(t_answer, pos, NUMBER)) { exit(1); } 
+
+    int result = left.value + right.value;
+    return result;
+}
+
+
+bool eat(std::vector<token>& tokens, int& pos, Token_Type expected) {
+    if (pos < tokens.size() && tokens[pos].type == expected) {
+        pos++;
+        return true;
+    }
+    std::cout << "\nError: Expected token type " << expected << " at position " << pos << "\n";
+    return false;
+}
+
 
 
