cf8072c5767825562b383ebeb38248a74352f018
ZuggRugg <shayne.tman@gmail.com>
2025-06-21 12:06:58 -0500

    added AST files

---
+++ AST.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "AST.h"
+
+int main(void) {
+
+  std::cout << "hello";
+
+}
---
+++ AST.h
@@ -0,0 +1,22 @@
+#ifndef AST_H
+#define AST_H
+
+#include <iostream>
+
+
+struct Node {
+  char value;
+};
+
+
+class AST  {
+private:
+  Node next;
+public:
+  AST();
+};
+
+
+
+
+#endif AST_H
